3196fe19c6f85070a26c504774982c219392df74
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-05-05 07:28:40 -0700

    created project layout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Chart, Slider} from './Component'
+const App = () => {
+  return (
+    <div>
+      <h1>APP</h1>
+      <Slider/>
+      <Chart/>
+    </div>
+  )
+}
+export default App;
