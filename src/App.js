e3df146660824154c435e05beb1721cc72d34f20
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-05-06 13:24:27 -0700

    succesfully added slider and doughnut bar components. Properly works

--- src/App.js
+++ src/App.js
@@ -8,15 +8,13 @@ const App = () => {
   const taxInDollars = salary * (taxPercentage)
   const netIncome = salary * (1-(taxPercentage))
   const monthlyIncome = parseInt((netIncome/12).toFixed(2));
+  const monthlyTax = (salary * taxPercentage)/12
 
-  
   return (
     <div>
       <input type='text' placeholder="Enter Earnings Per Year" value={salary} onChange={(e) => setSalary(e.target.value)}/>
       <input type='text' placeholder='Enter Tax'value={taxRate} onChange={(e) => setTaxRate(e.target.value)} />%
-      <button onClick={()=> console.log(netIncome, taxInDollars, monthlyIncome)}>button</button>
-
-      <Visuals salary={salary} taxInDollars={taxInDollars} netIncome={netIncome} monthlyIncome={monthlyIncome}/>
+      <Visuals monthlyIncome={monthlyIncome} monthlyTax={monthlyTax}/>
     </div>
   )
 }
--- src/Component/Visuals.jsx
+++ src/Component/Visuals.jsx
@@ -1,31 +1,58 @@
 import React,{useState} from 'react';
 import {Typography, Slider} from '@material-ui/core'
-// import {Doughnut} from 'react-chartjs-2';
+import {Doughnut} from 'react-chartjs-2';
 
 
-const Visuals = ({salary,taxinDollars, netIncome, monthlyIncome}) => {
-  const [rentBudget, setRentBudget] = useState('');
-  const rent = monthlyIncome * (rentBudget/100);
-  console.timeLog(rentBudget)
-  console.log(rent)
+const Visuals = ({monthlyIncome, monthlyTax}) => {
+  const [rentBudget, setRentBudget] = useState(30);
+  const [click, setClick] = useState(false)
+
+  const rent = parseInt(monthlyIncome * (rentBudget/100))
+  const monthlyIncomeAfterTaxAndRent = monthlyIncome - rent;
+
+  const data =  {
+    labels: [
+        'monthlyIncomeAfterTaxAndRent',
+        'Rent',
+        'Tax',
+    ],
+    datasets: [{
+        data: [monthlyIncomeAfterTaxAndRent, rent, monthlyTax],
+        backgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56',
+        // '#CC99FF'
+        ],
+        hoverBackgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56',
+        // '#CC99FF'
+        ]
+    }]
+};
   return (
     <>
-
-    <Typography id="discrete-slider-small-steps" gutterBottom>
-      Percentage of Monthly Income Going Towards Rent *Generally recommendation is up to 30%*
-    </Typography>
-    <Slider
-      defaultValue={0}
-      // getAriaValueText={valuetext}
-      aria-labelledby="discrete-slider-small-steps"
-      step={1}
-      // marks
-      min={-0.00000000}
-      max={100}
-      valueLabelDisplay="auto"
-      onChange={console.log('hi')}
-    />
-
+      <Typography id="discrete-slider-small-steps" gutterBottom>
+        Percentage of Monthly Income Going Towards Rent *Generally recommendation is up to 30%* *numbers rounded to nearest whole dollar*
+      </Typography>
+      <Slider
+        // defaultValue={30}
+        value={rentBudget}
+        // getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider-small-steps"
+        step={1}
+        // marks
+        min={-0.00000000}
+        max={100}
+        valueLabelDisplay="auto"
+        onChange={(e,value)=>setRentBudget(value)}
+      />
+      <button onClick={()=>setClick(true)} >CALCULATE BUDGET</button>
+        {click?(
+            <Doughnut data={data}/>
+        ):console.log('no click')}    
     </>
   )
 }
